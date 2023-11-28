a50091a997ef1bf21f53abfcff7e4c788b9db6af
Fabiano Avelar <fabianoavelar86@gmail.com>
2023-11-27 21:18:19 -0300

    Spring initializr

---
+++ src/main/java/br/com/ada/ifome/ifome/IfomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.ifome.ifome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfomeApplication.class, args);
+	}
+
+}
